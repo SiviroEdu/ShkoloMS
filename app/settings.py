1b577199729cec4ed26562acb08fb3f8af415cba
Kirill Ibragimov <hexchap@gmail.com>
2024-11-16 15:38:05 +0200

    Switch to seleniumbase

--- app/settings.py
+++ app/settings.py
@@ -2,8 +2,7 @@ import os
 
 import aiohttp
 from passlib.context import CryptContext
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from seleniumbase import Driver
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -12,8 +11,7 @@ db_url = os.environ["DB_URL"]
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
+# create a Driver instance with undetected_chromedriver (uc) and headless mode
+driver = Driver(headless=True)
 
 session = aiohttp.ClientSession()
