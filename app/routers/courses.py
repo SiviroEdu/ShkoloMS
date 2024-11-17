b486dec54ee426e481a5dbccacc14867b3c6fbb8
Kirill Ibragimov <hexchap@gmail.com>
2024-11-17 10:43:10 +0200

    add prefetch query param get courses

--- app/routers/courses.py
+++ app/routers/courses.py
@@ -1,8 +1,9 @@
+from re import purge
 from typing import Annotated
 
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, Query
 
-from app import UserSchema, PupilCRUD
+from app import UserSchema, PupilCRUD, Pupil
 from app.routers.auth import get_current_user
 
 router = APIRouter(
@@ -11,7 +12,13 @@ router = APIRouter(
 )
 
 @router.get("/@me")
-async def get_courses(user: Annotated[UserSchema, Depends(get_current_user)]):
-    pupil = await PupilCRUD.get_by(id=user.pupil_id)
+async def get_courses(
+        user: Annotated[UserSchema, Depends(get_current_user)],
+        prefetch: bool = Query(True)
+):
+    if prefetch:
+        pupil = await PupilCRUD.get_by(id=user.pupil_id)
+        return pupil.year_class.courses
 
-    return pupil.year_class.courses
+    pupil = await Pupil.get_or_none(id=user.pupil_id).prefetch_related("year_class__courses")
+    return await pupil.year_class.courses
